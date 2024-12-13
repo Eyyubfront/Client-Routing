b62a0fce4792a6c086dbfed2a2a9a2da8cee581c
Eyyubfront <eyyubbayramov811@gmail.com>
2024-12-13 10:31:43 +0400

    finisih

--- cl_routing/src/Pages/Home/index.jsx
+++ cl_routing/src/Pages/Home/index.jsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom"
+
 const Home=()=>{
     return(
         <>|
+<header>
+    <Link className="link" to="/">
+    <p>home </p>
+    </Link>
+    <Link className="link" to="/contact">
+    <p>contact </p>
+    </Link>
+    <Link className="link" to="/about">
+    <p>about </p>
+    </Link>
+</header>
+
         <div>
             bu vebsite  Eyyub haqqinda melumatlar ucundu
+
+            
         </div>
         
         </>
